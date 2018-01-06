2a80036ef2eb902f1f4534e6dfedbd837f889068
kandeh <alirezaknd@yahoo.com>
2018-01-06 23:07:22 +0330

    added client

---
+++ src/client/Team/TeamAI.java
@@ -0,0 +1,27 @@
+package client.Team;
+
+import Game.*;
+
+public class TeamAI extends client.AI {
+
+    @Override
+    public String getTeamName() {
+        return "RandomRat";
+    }
+
+    @Override
+    public void think(Game game) {
+        Rat myRat = game.getMyRat();
+        if(myRat.getCell().hasCheese()) {
+            myRat.eat();
+        } else {
+            switch((int) (Math.random() * 4)) {
+                case 0: myRat.moveUp();     break;
+                case 1: myRat.moveRight();  break;
+                case 2: myRat.moveDown();   break;
+                case 3: myRat.moveLeft();   break;
+            }
+        }
+    }
+    
+}
